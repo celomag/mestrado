ea0c5f9e561f2115f266d68b691c1de447f2167c
TIAGOPC\tiago <tiagotag91@gmail.com>
2021-04-29 18:40:19 -0300

    Refatorado código do updateCelulaInversa.js e adicionado comentários Adicionado comentários no paraPar.js Adicionado comentários no gerarMatriz.js Adionado radio e botão e teste no index.html Criado gerarInput.js

--- js/gerarMatriz.js
+++ js/gerarMatriz.js
@@ -1,47 +1,63 @@
 function gerarMatriz(nomeTabela, criterios) {
     
+    /*
+    Tentando obter um elemento com o nome da tabela que esta sendo informada
+    Caso obter quer dizer que já existe a tabela que esta tentando ser criada
+    */
     var novaTabela = document.getElementById("div"+nomeTabela);
 
+    //Validando se já existe a tabela a tabela informada no parametro
+    //Se existir não terá como criar a mesma novamente
     if(typeof(novaTabela) != 'undefined' && novaTabela != null){
         alert("DIV com nome: "+nomeTabela+" já existe!");
         return;
     }
 
+    //Criando uma DIV especifica para a tabela a ser criada
     var divTabelaInterna = document.createElement("div");
     divTabelaInterna.setAttribute("id", "div"+nomeTabela);
     tabelaCriteriosPrincipal.appendChild(divTabelaInterna);
 
+    //Criando a tabela de acordo com o nome fornecido
     var tabela = document.createElement("TABLE");
     tabela.setAttribute("id", "tb_"+nomeTabela);
     tabela.setAttribute("border", "1");
     tabela.setAttribute("color", "black");
 
+    //Adicionando a tabela a div criada anteriormente
     divTabelaInterna.appendChild(tabela);
 
+    //Criando o cabecalho da matriz e adicioando a primeira celula como vazio
     var cabecalho = tabela.createTHead();
     var linhaCabecalho = cabecalho.insertRow(0);
     var colunaCabecalho = linhaCabecalho.insertCell(-1);
     colunaCabecalho.innerHTML = "";
 
+    //Populando o cabecalho com cada texto presente na lista de criterios
     for (var i = 0; i < criterios.length; i++){
         colunaCabecalho = linhaCabecalho.insertCell(-1);
         colunaCabecalho.innerHTML = criterios[i];
     }
 
+    //Criando uma linha na tabela
     for (var x = 1; x <= criterios.length; x++) {
 
         var linha = tabela.insertRow(x);
 
+        //Acessando cada celula(ou coluna) da linha acima
         for(var y = 1; y <= criterios.length; y++){
 
+            //Criando um elemento select para adicionar na celula da linha acima
             var select = document.createElement("SELECT");
             select.setAttribute("id", tabela.id + "L" + x + "C" + y);
             select.setAttribute("name", "L" + x + "C" + y);
             select.setAttribute("onchange", "updateCelulaInversa(" + select.getAttribute("id") + ")" );
             
+            //Inserindo uma celula na linha e atribuindo o SELECT criado a esta celula
             var cell = linha.insertCell(-1);
             cell.appendChild(select);
 
+            //Verificando se a posicao atual é a diagonal e impedindo do usuario utiliza-la
             if(x == y){
                 var option = document.createElement("option");
                 var t = document.createTextNode(1);
@@ -50,7 +66,7 @@ function gerarMatriz(nomeTabela, criterios) {
                 document.getElementById(select.getAttribute("id")).disabled = true;
                 document.getElementById(select.getAttribute("id")).style.backgroundColor = "black";
             }
-            else{
+            else{//Adicionando opcao de 1 ate 9 no SELECT atual
                 for (var o = 1; o < 10; o++) {
                     var option = document.createElement("option");
                     option.setAttribute("value", o);
@@ -59,11 +75,12 @@ function gerarMatriz(nomeTabela, criterios) {
                     document.getElementById(select.getAttribute("id")).appendChild(option);
                 }
             }
-
+            /*Verificando se a posicao atual é a ultima da linha, caso for ira adicionar no inicio
+            uma celula contendo o nome da respectiva linha*/
             if(y == criterios.length){
                 var coluna = linha.insertCell(0);
-                console.log(criterios[x-1]);
-                coluna.innerHTML = criterios[x-1]; //Vetor inicia em 0, for esta iniciando em 1
+                console.log(criterios[x-1]); //Vetor inicia em 0, for esta iniciando em 1 por isso a correcao
+                coluna.innerHTML = criterios[x-1]; //Vetor inicia em 0, for esta iniciando em 1 por isso a correcao
             }
             
         }
--- js/paraPar.js
+++ js/paraPar.js
@@ -1,31 +1,39 @@
-function gerarParaPar(listaCriterios){
+function gerarParaPar(listaCriterios){ //Devera de ser informado OBRIGATORIAMENTE a lista de criterios
     
+    //Criando uma nova lista com apenas os valores selecionados
     var selectedCriterios = new Array();
 
+    //Percorrendo a lista original e verificando os valores selecionados
+    //Ao encontrar adiciona na lista de apenas valores selecionados
     for (var i = 0; i < listaCriterios.length; i++) {
         if (listaCriterios[i].checked) {
             selectedCriterios.push(listaCriterios[i].labels[0].innerHTML);
         }
     }
 
+    //Aplicando regra de limite de tamanho maximo e minimo
     if (selectedCriterios.length > 9 || selectedCriterios.length < 3){
         alert("Selecione no máximo 9 e no mínimo 3 critérios.");
         return
     }
     
+    //Obtendo a lista de radio pertencentes a mesmo grupo de radio
     var tipoAgrupamento = document.getElementsByName("radioClusterBairros");
 
+    //Verificando a lista qual dos radios esta selecionado para ser trabalho
     for(var i = 0; i < tipoAgrupamento.length; i++){
         if(tipoAgrupamento[i].checked){
             tipoAgrupamento = tipoAgrupamento[i].value;
         }
     }
     
+    //Validando se pelo menos um dos radios foi selecionado
     if(tipoAgrupamento != "Cluster" && tipoAgrupamento != "Bairro"){
         alert("Selecione um tipo de agrupamento!");
         return;
     }
 
+    //Efetuando validacao de acordo com o tipo de radio selecionado
     var tipoAgrupamentoMax;
 
     if(tipoAgrupamento == "Cluster"){
@@ -36,26 +44,30 @@ function gerarParaPar(listaCriterios){
         tipoAgrupamentoMax = 9;
     }
 
+
+    //Obtendo lista de todos os checkBox que sao pertencentes ao radio obtido anteriormente
     var listaAgrupamento = document.getElementsByName("check"+tipoAgrupamento);
 
     var selectedAgrupamento = new Array();
 
+    //Percorrendo todos os checkbox do radio e adicionado a uma nova lista apenas os selecionados
     for (var i = 0; i < listaAgrupamento.length; i++) {
         if (listaAgrupamento[i].checked) {
             selectedAgrupamento.push(listaAgrupamento[i].labels[0].innerHTML);
         }
     }
 
+    //Validando se o numero de checkbox selecionados esta dentro da regra
     if (selectedAgrupamento.length > tipoAgrupamentoMax || selectedAgrupamento.length < 3){
         alert("Selecione no máximo " + tipoAgrupamentoMax + " e no mínimo 3 para o agrupamento.");
         return
     }
 
-    //Adicionar título da matriz de critérios
+    //Gerando uma tabela de acordo com o nome informado e lista de dados
     gerarMatriz("criterios" , selectedCriterios);
 
+    //Gerando matriz de agrupamento de acordo com o numero de criterios informados
     for(var i = 0 ; i < selectedCriterios.length; i++){
-        //Adicionar título da matriz de agrupamento
         gerarMatriz(tipoAgrupamento + i, selectedAgrupamento);
     }
 
